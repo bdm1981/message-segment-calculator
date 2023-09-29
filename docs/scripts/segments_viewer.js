8ebff7d3af0fde298e8855d459d96f84579e8f67
Brad McAllister <bftr81Gmail.com>
2023-09-29 10:11:16 -0500

    updated site

--- docs/scripts/segments_viewer.js
+++ docs/scripts/segments_viewer.js
@@ -2,23 +2,17 @@ class SegmentsViewer {
     constructor(node, segmentTypesCount) {
         this.node = node;
         this.segmentTypesCount = segmentTypesCount;
-        this.twilioLogo = this.createTwilioLogo();
         this.blockMap = new Map();
         this.selectedBlocks = [];
     }
 
-    createTwilioLogo() {
-        let img = document.createElement('img');
-        const twilio_logo = '<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" preserveAspectRatio="xMidYMid meet"><path d="M15 0C6.7 0 0 6.7 0 15s6.7 15 15 15 15-6.7 15-15S23.3 0 15 0zm0 26C8.9 26 4 21.1 4 15S8.9 4 15 4s11 4.9 11 11-4.9 11-11 11zm6.8-14.7c0 1.7-1.4 3.1-3.1 3.1s-3.1-1.4-3.1-3.1 1.4-3.1 3.1-3.1 3.1 1.4 3.1 3.1zm0 7.4c0 1.7-1.4 3.1-3.1 3.1s-3.1-1.4-3.1-3.1c0-1.7 1.4-3.1 3.1-3.1s3.1 1.4 3.1 3.1zm-7.4 0c0 1.7-1.4 3.1-3.1 3.1s-3.1-1.4-3.1-3.1c0-1.7 1.4-3.1 3.1-3.1s3.1 1.4 3.1 3.1zm0-7.4c0 1.7-1.4 3.1-3.1 3.1S8.2 13 8.2 11.3s1.4-3.1 3.1-3.1 3.1 1.4 3.1 3.1z"/></svg>';
-        img.setAttribute("src", "data:image/svg+xml;base64," + btoa(twilio_logo));
-        return img;
-    }
-
-    createTwilioReservedCodeUnitBlock(segmentType) {
+    createReservedCodeUnitBlock(segmentType) {
         let block = document.createElement("div");
         block.setAttribute("class", `block twilio ${segmentType}`);
-        let twilioLogo = this.twilioLogo.cloneNode();
-        block.appendChild(twilioLogo);
+        let span = document.createElement('span');
+        span.textContent = "H";
+
+        block.appendChild(span);
         return block;
     }
 
@@ -52,7 +46,7 @@ class SegmentsViewer {
                 this.blockMap.set(mapKey, []);
 
                 if (encodedChar.isReservedChar) {
-                    newSegments.appendChild(this.createTwilioReservedCodeUnitBlock(segmentType));
+                    newSegments.appendChild(this.createReservedCodeUnitBlock(segmentType));
                 } else {
                     if (encodedChar.codeUnits) {
                         for (const codeUnit of encodedChar.codeUnits) {
